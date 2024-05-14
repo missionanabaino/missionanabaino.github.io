1f4af01aced6bc64703b07b74aa72a1083f95db0
agSuperCoder123 <alex@ahoy.ooo>
2024-05-14 15:16:37 -0400

    moved some things around not being used - trying to update the media library

--- src/omit/media-LoadMedia.js
+++ src/omit/media-LoadMedia.js
@@ -1,19 +1,21 @@
 document.addEventListener('DOMContentLoaded', function() {
-    fetch('./src/data/media/ma_mediaCollection.json') // Updated path
+    fetch('../data/media/ma_mediaCollection.json')
         .then(response => response.json())
         .then(data => {
             const mediaGrid = document.querySelector('.media-grid');
             data.forEach(item => {
+                const videoUrl = item.url;
+                const embedUrl = `https://www.youtube.com/embed/${videoUrl.split('v=')[1].split('&')[0]}`;
+
                 const mediaItem = document.createElement('div');
                 mediaItem.className = 'media-item';
                 mediaItem.innerHTML = `
-                    <a href="${item.url}" target="_blank">
-                        <img src="${item.thumbnail}" alt="Media Thumbnail">
-                        <p>${item.title}</p>
-                    </a>
+                    <iframe src="${embedUrl}" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
+                    <p>${item.title}</p>
                 `;
                 mediaGrid.appendChild(mediaItem);
             });
         })
         .catch(error => console.error('Error loading media items:', error));
 });
+
