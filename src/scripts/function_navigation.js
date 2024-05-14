6a09dc06425dc7a320d2195d7922c20566ca46ff
agSuperCoder123 <alex@ahoy.ooo>
2024-05-14 14:15:11 -0400

    basics of a slideshow

--- src/scripts/function_navigation.js
+++ src/scripts/function_navigation.js
@@ -1,8 +1,24 @@
-function toggleOverlay() {
-    var overlay = document.getElementById("overlayMenu");
-    if (overlay.style.width === '100%') {
-        overlay.style.width = '0%';
-    } else {
-        overlay.style.width = '100%';
-    }
+var slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
 }
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+  }
+  slides[slideIndex-1].style.display = "block";
+}
+
